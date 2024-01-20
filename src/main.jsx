fd87fcdf1172c518634db6b63625beed92c172f0
Mark Liu <877677447@qq.com>
2024-01-20 21:52:27 +0800

    feat: init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Root from './routes/root.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import ErrorPage from './error-page.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
