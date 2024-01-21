47d546398eb1741b69992c9c42b9915fb847cef7
lt <877677447@qq.com>
2024-01-21 18:42:14 +0800

    feat: add editContact

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Root from './routes/root.jsx'
+import Root, { loader as rootLoader, action as rootAction } from './routes/root.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import ErrorPage from './error-page.jsx'
+import Contact, { loader as contactLoader } from './routes/contact.jsx'
+import EditContact, { loader as editContactLoader } from './routes/edit.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
-  }
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact></Contact>,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact></EditContact>,
+        loader: editContactLoader,
+      }
+    ],
+  },
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,20 @@
+import { Outlet, Link, useLoaderData, Form } from 'react-router-dom'
+import { getContacts, createContact } from '../contacts'
+
+export async function action(paramObj) {
+    console.log('paramObj', paramObj);
+    const contact = await createContact()
+    return { contact }
+}
+
+export async function loader() {
+    console.log('加载全部联系人');
+    const contacts = await getContacts()
+    return { contacts }
+}
+
 export default function Root() {
+    const { contacts } = useLoaderData()
     return (
         <>
             <div id="sidebar">
@@ -22,22 +38,47 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
-                    <ul>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
+
+                    {/* <ul>
                         <li>
-                            <a href={`/contacts/1`}>Your Name</a>
+                            <Link to={`/contacts/1`}>Your Name</Link>
                         </li>
                         <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
+                            <Link to={`/contacts/2`}>Your Friend</Link>
                         </li>
-                    </ul>
+                    </ul> */}
                 </nav>
             </div>
-            <div id="detail"></div>
+            <div id="detail">
+                <Outlet></Outlet>
+            </div>
         </>
     );
 }
